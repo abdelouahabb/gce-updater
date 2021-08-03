8693d5c00499aff7972dd0c8c04fc2130ed3c51b
ALIANE Abdelouahab <abdelouahab_al@yahoo.fr>
2021-08-03 04:09:27 +0100

    added some debug trick

--- copier.sh
+++ copier.sh
@@ -5,5 +5,6 @@ echo "__________the script begins here____________"
 
 scp app.py handlers.py refresh.sh ${SA}@${HOST}:/home/${SA}/
 ssh ${SA}@${HOST}
+echo "if you see this in the build log, that means that i am inside the machine ^_^ "
 
 echo "__________the script ends here____________"
