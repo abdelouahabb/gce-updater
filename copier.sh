b87ecb3c4194e56f90ef44ed5763e8ffec69805c
ALIANE Abdelouahab <abdelouahab_al@yahoo.fr>
2021-08-03 04:03:34 +0100

    Update copier.sh

--- copier.sh
+++ copier.sh
@@ -4,5 +4,6 @@ HOST="YOUR_GCE_VM_PUBLIC_IP"
 echo "__________the script begins here____________"
 
 scp app.py handlers.py refresh.sh ${SA}@${HOST}:/home/${SA}/
+ssh ${SA}@${HOST}
 
 echo "__________the script ends here____________"
